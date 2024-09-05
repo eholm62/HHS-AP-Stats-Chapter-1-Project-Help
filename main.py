aa6c6a2f4299164af0be3763a8d30d5bc29ea374
Elliot Holmes <133877996+eholm62@users.noreply.github.com>
2024-09-04 21:01:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import csv
 
+# must download these files and name them properly before use of program
+# get files from https://survey.stackoverflow.co/
+
+data2020 = []
+with open("survey_results_public_2020.csv") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        try:
+            data2020.append(float(row[6]))
+        except:
+            print("Non Numeric Value: " + row[6])
+
+
+data2024 = []
+with open("survey_results_public_2024.csv") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        try:
+            data2024.append(float(row[21]))
+        except:
+            print("Non Numeric Value: " + row[21])
+
+
+print(data2020)
+print(data2024)
