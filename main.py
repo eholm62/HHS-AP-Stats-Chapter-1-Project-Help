fd9e38179a904dd9709053a01146c8ce4d2a65e1
Elliot Holmes <133877996+eholm62@users.noreply.github.com>
2024-09-05 21:35:42 +0000

    finished the program so that it successfully culls all extreme values such that the data is able to be analyzed, Each sample is culled then output to a csv file, with a line per sample

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import csv
+import numpy as np
 
-# must download these files and name them properly before use of program
-# get files from https://survey.stackoverflow.co/
+import stats
 
+# must download these files and name them appropiately based on the year
+# before use of program
+# obtain files from https://survey.stackoverflow.co/
+
+# 6 is the column including the salary data 
+nonNumericData2020 = []
 data2020 = []
 with open("survey_results_public_2020.csv") as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         try:
-            data2020.append(float(row[6]))
+            data2020.append(float(row[6])) 
         except:
-            print("Non Numeric Value: " + row[6])
+            nonNumericData2020.append(row[6])
 
 
+# 21 is the column including the salary data
+nonNumericData2024 = []
 data2024 = []
 with open("survey_results_public_2024.csv") as csvfile:
     reader = csv.reader(csvfile)
@@ -20,8 +28,26 @@ with open("survey_results_public_2024.csv") as csvfile:
         try:
             data2024.append(float(row[21]))
         except:
-            print("Non Numeric Value: " + row[21])
+            nonNumericData2024.append(row[21])
 
 
-print(data2020)
-print(data2024)
+data2020.sort()
+data2024.sort()
+
+sample2020 = np.random.choice(data2020, 10000)
+sample2024 = np.random.choice(data2024, 10000)
+
+fiveNum2020 = stats.getFiveNumSummary(data2020)
+fiveNum2024 = stats.getFiveNumSummary(data2024)
+
+filteredSample2020 = list(filter(lambda x: not stats.isOutlier(x, fiveNum2020), sample2020))
+filteredSample2024 = list(filter(lambda x: not stats.isOutlier(x, fiveNum2024), sample2024))
+
+with open("processed_data.csv", "w") as dataFile:
+    for dataPoint in filteredSample2020:
+        dataFile.write(str(dataPoint) + ", ")
+    dataFile.write("\n")
+
+    for dataPoint in filteredSample2024:
+        dataFile.write(str(dataPoint) + ", ")
+    dataFile.write("\n")
---
+++ stats.py
@@ -0,0 +1,59 @@
+# takes in an array and an index as a float or int,
+# and returns the value of the array at that index
+# where a non-integer value represents a weighted average
+# of the closest two values based on how close the float is
+# to each of those indicies
+def getWeightedValueAt(i, arr):
+    if i < 0:
+        i = len(arr) + i
+    if i % 1 == 0:
+        return arr[int(i)]
+    return arr[int(i)] * (i % 1) + arr[int(i) + 1] * (1 - i % 1)
+
+# returns the five number summmary given a list of values
+def getFiveNumSummary(arr):
+    arr = arr.copy()
+    arr.sort()
+    
+    if len(arr) % 2 != 0:
+        posMedian = (len(arr) + 1) // 2 - 1
+        posQ1 = (posMedian) / 2
+        posQ3 = -posQ1 - 1
+    else:
+        posMedian = (len(arr) + 1) / 2 - 1
+        posQ1 = (int(posMedian)) / 2
+        posQ3 = -posQ1 - 1
+    
+    q1 = getWeightedValueAt(posQ1, arr)
+    median = getWeightedValueAt(posMedian, arr)
+    q3 = getWeightedValueAt(posQ3, arr)
+    iqr = q3 - q1
+    
+    i = 0
+    while q1 - 1.5 * iqr > arr[i]:
+        i += 1
+    posMin = i
+
+    i = -1
+    while arr[i] > q3 + 1.5 * iqr:
+        i -= 1
+    posMax = i
+    
+    return (
+        arr[posMin],
+        q1,
+        median,
+        q3,
+        arr[posMax]
+    )
+
+# returns a boolean representing whether or not x is 
+# an outlier given the five number summary, using
+# the 1.5 * IQR method
+def isOutlier(x, fiveNumSummary):
+    iqr = (fiveNumSummary[3] - fiveNumSummary[1])
+    if x < fiveNumSummary[1] - 1.5 * iqr:
+        return True
+    elif x > fiveNumSummary[3] + 1.5 * iqr:
+        return True
+    return False
